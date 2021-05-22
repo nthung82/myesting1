a4b25a09be4ab4275d7fd7acea2e94cffb92cd01
Nguyen Hung <you@example.com>
2021-05-22 23:27:39 +0700

    asdasd

--- src/main/java/com/mydomain/controller/LoginController.java
+++ src/main/java/com/mydomain/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-	
+	//////////
 	@RequestMapping(method = RequestMethod.GET)
 	public String login() {
 		return "redirect:/dummy";
